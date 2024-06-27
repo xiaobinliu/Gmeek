9e63d5824ff0eb1ab37b2c6e32f9c4763f217727
刘小斌 <hi@lius.us>
2024-06-27 10:49:08 +0800

    Update Gmeek.py

--- Gmeek.py
+++ Gmeek.py
@@ -484,7 +484,7 @@ if os.environ.get('GITHUB_EVENT_NAME')!='schedule':
     readme=readme+"### :speech_balloon: %d \r\n" % commentNumSum
     readme=readme+"### :hibiscus: %d \r\n" % wordCount
     readme=readme+"### :alarm_clock: %s \r\n" % datetime.datetime.now(blog.TZ).strftime('%Y-%m-%d %H:%M:%S')
-    readme=readme+"### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)\r\n"
+    readme=readme+"### Powered by :heart: [Gmeek](https://github.com/xiaobinliu/Gmeek)\r\n"
     readmeFile=open(workspace_path+"/README.md","w")
     readmeFile.write(readme)
     readmeFile.close()
